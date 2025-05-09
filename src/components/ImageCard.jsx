050872ff1b7bd94e5f41d9c2fa949b96fc1466c9
NanamiNihon <89219825+NanamiNihon@users.noreply.github.com>
2025-05-09 07:32:44 +0000

    minor styling

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ImageCard(){
     return(
         <div>
-            <img src="https://tse2.mm.bing.net/th?id=OIP.oJuOblM84fHw3g2YGEolRQHaFX&cb=iwp1&pid=Api" alt="Birdo" />
+            <img className="card" src="https://tse2.mm.bing.net/th?id=OIP.oJuOblM84fHw3g2YGEolRQHaFX&cb=iwp1&pid=Api" alt="Birdo" />
         </div>
     );
 }
