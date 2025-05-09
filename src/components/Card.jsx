5ee3de4bdbed27e605425ecd4eaf51f76e30de29
NanamiNihon <89219825+NanamiNihon@users.noreply.github.com>
2025-05-09 07:12:05 +0000

    minor changes to styles and card element

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,7 @@ import ImageCard from "./ImageCard";
 function Card() {
     return(
         <div className="card-element">
-            <strong>username</strong> <p>handle</p> <p>timestamp</p>
-            <br />
+            <div className="post-creator"><strong>username</strong> <p>handle</p> <p>timestamp</p></div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ullam temporibus doloribus voluptatum delectus officia veniam modi rerum possimus, quos tempore. Provident velit blanditiis temporibus natus voluptas fugiat consequuntur rem.</p>
             <ImageCard />
         </div>
