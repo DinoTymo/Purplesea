46147cb08637295ea2fdce6516ba77d003bdba8f
NanamiNihon <89219825+NanamiNihon@users.noreply.github.com>
2025-05-12 12:45:51 +0000

    lots of stuff (mostly styling and positioning), icons dont really work need to fix that

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,27 @@ import ImageCard from "./ImageCard";
 
 function Card() {
     return(
-        <div className="card">
-            <div className="post-creator"><strong>username</strong> <p>handle</p> <p>timestamp</p></div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ullam temporibus doloribus voluptatum delectus officia veniam modi rerum possimus, quos tempore. Provident velit blanditiis temporibus natus voluptas fugiat consequuntur rem.</p>
-            <ImageCard />
+        <div className="card mx-auto p-2">
+            <div className="card-header post-creator d-flex flex-row mb-3">
+                <strong>username</strong> <p>handle</p> <p>timestamp</p>
+            </div>
+            <div className="card-body">
+                <div className="card-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ullam temporibus doloribus voluptatum delectus officia veniam modi rerum possimus, quos tempore. Provident velit blanditiis temporibus natus voluptas fugiat consequuntur rem.</p>
+                    <ImageCard />
+                </div>
+                <div className="card-footer d-flex justify-content-center">
+                    <button className="btn btn-primary">
+                        like
+                    </button>
+                    <button className="btn btn-outline-primary">
+                        repost
+                    </button>
+                    <button className="btn btn-outline-primary">
+                        comment
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
