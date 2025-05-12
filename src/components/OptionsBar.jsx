fc3d831de67fbfb2f59aa73375f23add4f0494d0
Dino <64032626+DinoTymo@users.noreply.github.com>
2025-05-12 12:32:06 +0000

    Add Bootstrap Icons

--- src/components/OptionsBar.jsx
+++ src/components/OptionsBar.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function OptionsBar() {
   return (
     <div className="options-bar">
-      <i></i>
-      <i></i>
-      <i></i>
-      <i></i>
+      <FontAwesomeIcon icon="fa-regular fa-image" />
+      <i class="bi bi-film"></i>
+      <FontAwesomeIcon icon="fa-solid fa-house" />
+      <FontAwesomeIcon icon="fa-solid fa-house" />
     </div>
   );
 }
