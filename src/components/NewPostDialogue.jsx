baee3de87a6cd149617483727c6a2c7999ec8dea
Dino <64032626+DinoTymo@users.noreply.github.com>
2025-05-15 06:47:28 +0000

    Add right Icons

--- src/components/NewPostDialogue.jsx
+++ src/components/NewPostDialogue.jsx
@@ -4,10 +4,6 @@ import OptionsBar from "./OptionsBar";
 function NewPostDialogue() {
   return (
     <div className="new-post-dialogue">
-      <button type="button" className="btn btn-primary">
-        Base class
-      </button>
-      <i class="bi bi-film"></i>
       <OptionsBar />
     </div>
   );
--- src/components/OptionsBar.jsx
+++ src/components/OptionsBar.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faImage } from "@fortawesome/free-solid-svg-icons"
+import "bootstrap-icons/font/bootstrap-icons.css";
+import GifIcon from "./icons/GifIcon";
 
 function OptionsBar() {
   return (
     <div className="options-bar">
-      <i class="bi bi-film"></i>
-      <i class="bi bi-image"></i>
-      <FontAwesomeIcon icon={faImage} />
+      <i class="bi bi-image text-primary"></i>
+      <i class="bi bi-film text-primary"></i>
+      <GifIcon width="2" height="2" />
     </div>
   );
 }
