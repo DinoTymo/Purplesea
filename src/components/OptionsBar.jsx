d9e17d83957818c1bb7e597709698edc377569ff
Dino <64032626+DinoTymo@users.noreply.github.com>
2025-05-19 13:02:48 +0000

    Update .gitignore to include package-lock.json and correct bootstrap.css path; add bootstrap.scss for custom styles

--- src/components/OptionsBar.jsx
+++ src/components/OptionsBar.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import "bootstrap-icons/font/bootstrap-icons.css";
+import "../../public/styles/bootstrap-icons.css";
 import GifIcon from "./icons/GifIcon";
 
 function OptionsBar() {
   return (
     <div className="options-bar">
-
       <div className="attachmentPicker">
         <i class="bi bi-image text-primary"></i>
         <i class="bi bi-film text-primary"></i>
@@ -13,7 +12,6 @@ function OptionsBar() {
       </div>
 
       <div className="contentOptions"></div>
-
     </div>
   );
 }
