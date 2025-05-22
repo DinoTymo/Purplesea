afdc39a307662da6a3461127df9383798bbdda0d
Dino <64032626+DinoTymo@users.noreply.github.com>
2025-05-22 20:55:58 +0200

    I realised, I forgot to git add Header.jsx 🤦‍️

---
+++ src/components/NewPostDialogue/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="new-post-dialogue-header card-header">
+      <div className="text-primary fw-bold">Cancel</div>
+      <div className="btn btn-primary rounded-pill fw-bold">Post</div>
+    </div>
+  );
+}
+
+export default Header;
