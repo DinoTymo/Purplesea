9ad4bd671f7b3c81ae79c98c920bd223249f594f
nanami nihon <testemail@mail.com>
2025-05-23 21:41:11 +0200

    changes to card element

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,34 +6,49 @@ import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faRepeat } from "@fortawesome/free-solid-svg-icons";
 import { faMessage } from "@fortawesome/free-regular-svg-icons";
 
-function Card() {
+function Card(props) {
     return(
         <div className="card mx-auto p-2">
             <div className="card-header post-creator d-flex flex-row mb-3">
-                <Avatar /> <strong>&nbsp;&nbsp;&nbsp;username</strong> <p>&nbsp;handle</p> <p>&nbsp;timestamp</p>
+                <Avatar /> <strong>&nbsp;&nbsp;&nbsp;{props.username}</strong> <p>&nbsp;{props.handle}</p> <p>&nbsp;{props.timestamp}</p>
             </div>
             <div className="card-body">
                 <div className="card-text">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ullam temporibus doloribus voluptatum delectus officia veniam modi rerum possimus, quos tempore. Provident velit blanditiis temporibus natus voluptas fugiat consequuntur rem.</p>
+                    <p>{props.postContent}</p>
                     <ImageCard />
                 </div>
                 <div className="card-footer d-flex justify-content-around">
                     <button className="btn btn-outline-primary d-flex">
                         <FontAwesomeIcon icon={faMessage} />
-                        <div className="commentCount">0</div>
+                        <div className="commentCount">{randomNumbersComments()}</div>
                     </button>
                     <button className="btn btn-outline-primary d-flex">
                         <FontAwesomeIcon icon={faRepeat} />
-                        <div className="repostCount">0</div>
+                        <div className="repostCount">{randomNumbersReposts()}</div>
                     </button>
                     <button className="btn btn-outline-primary d-flex">
                         <FontAwesomeIcon icon={faHeart} />
-                        <div className="likeCount">0</div>
+                        <div className="likeCount">{randomNumbersLikes()}</div>
                     </button>
                 </div>
             </div>
         </div>
     );
+}
+
+function randomNumbersLikes(){
+    let number = Math.floor(Math.random() * 4501);
+    return number;
+}
+
+function randomNumbersReposts(){
+    let number = Math.floor(Math.random() * 171);
+    return number;
+}
+
+function randomNumbersComments(){
+    let number = Math.floor(Math.random() * 1281);
+    return number;
 }
 
 export default Card;
