42dfd6ccb89d45a81ddf6a27a2086682daa6bccd
nanami nihon <testemail@mail.com>
2025-05-23 22:06:52 +0200

    minor changes to app and body

---
+++ src/components/Body.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Card from "./Card";
+
+function Body() {
+    return(
+        <div>
+            <Card username="urmum" handle="urmum@psea.social" timestamp="2 Std."/>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    )
+}
+
+
+export default Body;
