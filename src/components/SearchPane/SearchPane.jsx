f92e2240bd7a502eaad22c01d389b8f114ae447e
nanami nihon <testemail@mail.com>
2025-05-24 16:09:44 +0200

    beginning with search pane

--- src/components/SearchPane/SearchPane.jsx
+++ src/components/SearchPane/SearchPane.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function SearchPane() {
-  return <div className="search-pane"></div>;
+  return (
+    <div className="search-pane">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <br />
+        <a href="#">Discover</a>
+        <br />
+        <a href="#">Following</a>
+        <br />
+        <a href="#">More Feeds</a>
+        <br />
+        <span className="border-bottom">testetsetsetetstestetstestestetset</span>
+        <br />
+        <p>Trending</p>
+        <br />
+        <span className="border-bottom">testetsetsetetstestetstestestetset</span>
+        <br />
+        <a href="#">Feedback</a> <br />
+        <a href="#">Datenschutz</a>
+        <br />
+        <a href="#">Bedingungen</a> <br />
+        <a href="#">Hilfe</a>
+      </div>
+    </div>
+  );
 }
 
 export default SearchPane;
