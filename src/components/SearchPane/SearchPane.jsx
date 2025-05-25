4963549c22941dca6e5d28113a2a577f5578ea89
nanami nihon <testemail@mail.com>
2025-05-25 21:18:01 +0200

    Refactor SearchPane: add padding, enhance search input styling, and maintain link structure

--- src/components/SearchPane/SearchPane.jsx
+++ src/components/SearchPane/SearchPane.jsx
@@ -4,28 +4,54 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function SearchPane() {
   return (
-    <div className="search-pane sticky-top">
+    <div className="search-pane p-4 sticky-top">
       <div className="search-bar">
-        <FontAwesomeIcon icon={faMagnifyingGlass} className="sideIcon" />
-        <input type="text" placeholder="Search" />
+        <div class="input-group flex-nowrap">
+          <span
+            class="input-group-text"
+            id="addon-wrapping"
+            style={{ background: "#161e27" }}
+          >
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="sideIcons" />
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Search"
+            aria-label="Search"
+            aria-describedby="addon-wrapping"
+            style={{ background: "#161e27", color: "#eee" }}
+          />
+        </div>
         <br />
-        <a href="#" className="link-underline link-underline-opacity-0">Discover</a>
+        <a href="#" className="link-underline link-underline-opacity-0">
+          Discover
+        </a>
         <br />
-        <a href="#" className="link-underline link-underline-opacity-0">Following</a>
+        <a href="#" className="link-underline link-underline-opacity-0">
+          Following
+        </a>
         <br />
-        <a href="#" className="link-underline link-underline-opacity-0">More Feeds</a>
+        <a href="#" className="link-underline link-underline-opacity-0">
+          More Feeds
+        </a>
+        <div className="hr"></div>
         <br />
-        <span className="border-bottom">testetsetsetetstestetstestestetset</span>
+        <a href="#" className="link-underline link-underline-opacity-0">
+          Feedback
+        </a>
         <br />
-        <p>Trending</p>
+        <a href="#" className="link-underline link-underline-opacity-0">
+          Datenschutz
+        </a>
         <br />
-        <span className="border-bottom">testetsetsetetstestetstestestetset</span>
+        <a href="#" className="link-underline link-underline-opacity-0">
+          Bedingungen
+        </a>
         <br />
-        <a href="#" className="link-underline link-underline-opacity-0">Feedback</a> <br />
-        <a href="#" className="link-underline link-underline-opacity-0">Datenschutz</a>
-        <br />
-        <a href="#" className="link-underline link-underline-opacity-0">Bedingungen</a> <br />
-        <a href="#" className="link-underline link-underline-opacity-0">Hilfe</a>
+        <a href="#" className="link-underline link-underline-opacity-0">
+          Hilfe
+        </a>
       </div>
     </div>
   );
