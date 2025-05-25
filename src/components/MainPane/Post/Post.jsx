3d277910677eb251e6d4fa3af675c2fcad14b39f
nanami nihon <testemail@mail.com>
2025-05-25 11:39:54 +0200

    minor changes (props added (again))

--- src/components/MainPane/Post/Post.jsx
+++ src/components/MainPane/Post/Post.jsx
@@ -6,35 +6,32 @@ import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { faRepeat } from "@fortawesome/free-solid-svg-icons";
 import { faMessage } from "@fortawesome/free-regular-svg-icons";
 
-function Post() {
+function Post(props) {
   return (
     <div className="card mx-auto p-2 text-bg-dark border border-dark-subtle">
       <div className="card-header post-creator d-flex flex-row mb-3">
-        <Avatar /> <strong>&nbsp;&nbsp;&nbsp;username</strong>{" "}
-        <p>&nbsp;handle</p> <p>&nbsp;timestamp</p>
+        <Avatar /> <strong>&nbsp;&nbsp;&nbsp;{props.username}</strong>{" "}
+        <p>&nbsp;{props.handle}</p> <p>&nbsp;{props.timestamp}</p>
       </div>
       <div className="card-body">
         <div className="card-text">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ullam
-            temporibus doloribus voluptatum delectus officia veniam modi rerum
-            possimus, quos tempore. Provident velit blanditiis temporibus natus
-            voluptas fugiat consequuntur rem.
+            {props.content}
           </p>
           <ImageCard />
         </div>
         <div className="card-footer d-flex justify-content-around">
           <button className="btn btn-outline-primary d-flex">
             <FontAwesomeIcon icon={faMessage} />
-            <div className="commentCount">0</div>
+            <div className="commentCount">{props.comments}</div>
           </button>
           <button className="btn btn-outline-primary d-flex">
             <FontAwesomeIcon icon={faRepeat} />
-            <div className="repostCount">0</div>
+            <div className="repostCount">{props.reposts}</div>
           </button>
           <button className="btn btn-outline-primary d-flex">
             <FontAwesomeIcon icon={faHeart} />
-            <div className="likeCount">0</div>
+            <div className="likeCount">{props.likes}</div>
           </button>
         </div>
       </div>
--- src/components/MainPane/PostPane.jsx
+++ src/components/MainPane/PostPane.jsx
@@ -4,9 +4,9 @@ import Post from "./Post/Post";
 function PostPane() {
   return (
     <div className="post-pane">
-      <Post username="urmum" handle="urmum@psea.social" timestamp="2 Std." />
-      <Post username="urmum" handle="urmum@psea.social" timestamp="2 Std." />
-      <Post username="urmum" handle="urmum@psea.social" timestamp="2 Std." />
+      <Post username="" handle="urmum@psea.social" timestamp="2 Std." content="das in der json halt" comments="154" reposts="10" likes="354" />
+      <Post username="urmum" handle="urmum@psea.social" timestamp="2 Std." content="das in der json halt" comments="74" reposts="2" likes="98" />
+      <Post username="urmum" handle="urmum@psea.social" timestamp="2 Std." content="das in der json halt" comments="45" reposts="68" likes="451" />
     </div>
   );
 }
