10d31a29fdb34e001fa342ae1c79fd77c10b4e6b
Dino <64032626+DinoTymo@users.noreply.github.com>
2025-05-25 22:33:22 +0200

    Add secondary color and update PostHeader to fetch username

--- src/components/MainPane/Post/Post.jsx
+++ src/components/MainPane/Post/Post.jsx
@@ -3,14 +3,9 @@ import PostHeader from "./PostHeader";
 import PostBody from "./PostBody";
 
 function Post(props) {
-  // TODO: Username etc.
   return (
     <div className="post card mx-auto p-2 text-bg-dark border-dark-subtle">
-      <PostHeader
-        handle={props.handle}
-        username={props.username}
-        timestamp={props.timestamp}
-      />
+      <PostHeader handle={props.handle} timestamp={props.timestamp} />
       <PostBody
         comments={props.comments}
         likes={props.likes}
--- src/components/MainPane/Post/PostHeader.jsx
+++ src/components/MainPane/Post/PostHeader.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProfilePic from "../../ProfilePic";
 
 function PostHeader(props) {
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/users")
+      .then((response) => response.json())
+      .then((userResults) => {
+        const foundUser = userResults.find(
+          (userResult) => userResult.handle === props.handle,
+        );
+        setUsername(foundUser.name);
+      });
+  }, [props.handle]);
+
+  if (!username) return null;
+
   return (
     <div className="card-header post-creator d-flex flex-row mb-3">
-      <ProfilePic handle={props.handle} size="42" />{" "}
-      <strong>&nbsp;&nbsp;&nbsp;{props.username}</strong>
-      <p>&nbsp;{props.handle}</p> <p>&nbsp;{props.timestamp}</p>
+      <ProfilePic handle={props.handle} size="42" />
+      <div className="fw-bold ms-2">{username}</div>
+      <div className="text-secondary ms-1">@{props.handle}</div>
+      <div className="text-secondary ms-1">·</div>
+      <div className="text-secondary ms-1">{props.timestamp}</div>
     </div>
   );
 }
