1050b44fbc23173c952013d93697021a8d1e5f08
nanami nihon <testemail@mail.com>
2025-05-25 16:50:41 +0200

    bug fixing gone wrong

--- src/components/MainPane/LogoBar.jsx
+++ src/components/MainPane/LogoBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function LogoBar() {
   return (
     <div className="logo-bar">
-      <img src="public/assets/logo.png" alt="logo" />
+      <img src="/assets/logo.png" alt="logo" className="logoBar"/>
     </div>
   );
 }
--- src/components/MainPane/PostPane.jsx
+++ src/components/MainPane/PostPane.jsx
@@ -1,25 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Post from "./Post/Post";
-import $ from "jquery";
 
 function PostPane() {
-  $.ajax({
-    url: '/data/posts.json',
-    dataType: 'json',
-    success: function(data) {
-      console.log(data);
-      
-      return (
-      <div className="post-pane">
-        <Post username="urmum" handle={data[0].user} timestamp="2 Std." content="das in der json halt" comments="154" reposts="10" likes="354" />
-        <Post username="urmum" handle="urmum@psea.social" timestamp="2 Std." content="das in der json halt" comments="74" reposts="2" likes="98" />
-        <Post username="urmum" handle="urmum@psea.social" timestamp="2 Std." content="das in der json halt" comments="45" reposts="68" likes="451" />
-      </div>
-      );
-    }
-  });
-
+  console.log("hi");
   
+  const [post, setPost] = useState(null);
+  useEffect(()=>{
+    fetch("/data/posts.json")
+      .then((response)=>{
+        response.json();
+        console.log(response);
+      })
+      .then((posts)=>{
+        setPost(posts);
+      })
+  });
+  
+  if(!post){
+    return null;
+  }  
+
+  return (    
+    <div className="post-pane">
+      <p>muhahahahah</p>
+      <Post username="urmum" handle={post[0].user} timestamp="2 Std." content="das in der json halt" comments="154" reposts="10" likes="354" />
+      <Post username="urmum" handle="urmum@psea.social" timestamp="2 Std." content="das in der json halt" comments="74" reposts="2" likes="98" />
+      <Post username="urmum" handle="urmum@psea.social" timestamp="2 Std." content="das in der json halt" comments="45" reposts="68" likes="451" />
+    </div>
+  );  
 }
 
 export default PostPane;
