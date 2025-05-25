c8d642d4be52fa9ba4b318c04cfba080be20d8c6
nanami nihon <testemail@mail.com>
2025-05-25 23:36:57 +0200

    Fix layout by adding user details to the Profile component

--- src/components/MainPane/Profile/Profile.jsx
+++ src/components/MainPane/Profile/Profile.jsx
@@ -10,7 +10,7 @@ function Profile() {
       </div>
       <div className="w-100">
         <img style={{width: "auto", height: "50px"}} src="https://avatar.iran.liara.run/public/boy?username=Hans+Oßner" alt="Hans Oßner" />
-        
+        <span style={{color: "white", fontWeight: "bolder"}}>&nbsp;Hans Oßner</span><span style={{color: "white"}}>&nbsp;hans.kisten-und-co-kg.de</span>
       </div>
       <PostPane />
     </div>
